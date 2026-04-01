44afae14075dade1d9e72fd9810c16cdc95299c1
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2026-04-01 17:47:30 -0500

    feat/add dynamic project scroll tracker, and modify its responsive css

--- frontend/projectTracker.js
+++ frontend/projectTracker.js
@@ -5,9 +5,19 @@ const observer = new IntersectionObserver ((entries) => {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
 			const index = Array.from(projectCardsNodeList).indexOf(entry.target) + 1;
-			projectCount.innerHTML = `<i class='fas fa-layer-group'></i> Project ${index} of ${projectCardsNodeList.length}`;
+			projectCount.innerHTML = `<div class="timelineDotProject"></div> <div> Project ${index} of ${projectCardsNodeList.length} </div>`;
 		}
 	});
 }, {threshold: 0.9});
 
+const projectSection = document.getElementById('projects');
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      projectCount.innerHTML = "<div class='timelineDotProject'></div> <div>Total: 6 Projects</div>";
+    }
+  });
+}, { threshold: 0.2 });
+
+sectionObserver.observe(projectSection);
 projectCardsNodeList.forEach(card => observer.observe(card));
